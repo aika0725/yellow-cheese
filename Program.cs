938b5a4426f5425629bd73939d5e9372ba0d4026
Kaiying <32117865+aika0725@users.noreply.github.com>
2023-02-14 14:07:01 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yellow_cheese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string i = "33";
+            double f = 1234.56789;
+            Console.WriteLine("i: {0,10:0.0} f: {1,15:0.00}", i, f);
+            Console.WriteLine("i: {0,10:0.0} f: {1,15:0.00}", 0, 0);
+
+
+            Console.WriteLine("i: {0,10:0} f: {1,10:0.00}", i, f);
+            Console.WriteLine("i: {0,10:0} f: {1,10:0.00}", 0, 0);
+
+            Console.WriteLine("i: {0,-6:0} f: {1,-10:0.00}", i, f);
+            Console.WriteLine("i: {0,10:0} f: {1,-10:0.00}", 0, 0);
+            Console.ReadKey();
+
+
+    }
+}
+}
